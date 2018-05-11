f8a05149b9683eb74c8d1d398fe1bd74e10b82bf
Kiril <kiril.sotirovski@yahoo.com>
2018-05-11 04:02:06 +0200

    made navbar-fixed-top, added one page nav, improved mobile menu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
+$(window).scroll(function() {
+    var top = $(window).scrollTop();
+    if(top>=60)
+    {
+        $(".navbar-default").css("background-color", "#292c2f");
+    }
+    else if ($(".navbar-default").css("background-color", "#292c2f")) {
+        $(".navbar-default").css("background-color", "transparent");
+    }
+});
+    
     // ANIMATIONS
     new WOW().init();
 
     /* SMOOTH SCROLLING */
 $('a').smoothScroll();
+
+// ONE PAGE NAV
+$('#menu').onePageNav({
+	currentClass: 'active',
+	changeHash: false,
+	scrollSpeed: 750,
+	scrollThreshold: 0.5,
+	filter: '',
+	easing: 'swing'
+	
+});
